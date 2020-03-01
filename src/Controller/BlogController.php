ca55abbb6f70b84fae86d872051bfdb39ef5682d
Mael <contact.squeezo1@gmail.com>
2020-03-01 19:20:46 +0100

    Ajout de la partie blog (backend)
    - Affiche la liste des articles (active)
    - Affiche le poste concerné lorsque l'on clique dessus
    - Ajouts de méthode personnalisées pour récuperer les articles
    - Ajouts d'un côté fronted pour l'affichage des articles (liste et article concerné)

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,17 +2,49 @@
 
 namespace App\Controller;
 
+use App\Entity\Blog;
+use App\Repository\BlogRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 class BlogController extends AbstractController {
 
     /**
+     * @var BlogRepository
+     */
+    private $blogRepository;
+
+    public function __construct(BlogRepository $blogRepository) {
+        $this->blogRepository = $blogRepository;
+    }
+
+    /**
      * @return Response
      */
     public function index(): Response {
+        $post = $this->blogRepository->findLatest();
         return $this->render('pages/blog/blog.index.html.twig', [
-            'current_menu' => 'blog'
+            'current_menu' => 'blog',
+            'posts' => $post
+        ]);
+    }
+
+    /**
+     * @param Blog $post
+     * @param string $slug
+     * @return Response
+     */
+    public function show(Blog $post, string $slug): Response {
+        $getSlug = $post->getSlug();
+        if ($getSlug !== $slug) {
+            return $this->redirectToRoute('blog.show', [
+                'id' => $post->getId(),
+                'slug' => $getSlug
+            ], 301);
+        }
+        return $this->render('pages/blog/blog.show.html.twig', [
+            'current_menu' => 'blog',
+            'post' => $post
         ]);
     }
 }
---
+++ src/Entity/Blog.php
@@ -0,0 +1,163 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Cocur\Slugify\Slugify;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\BlogRepository")
+ */
+class Blog
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $slug;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $image;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $author;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created_at;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $edited_at;
+
+    /**
+     * @ORM\Column(type="boolean", options={"default": true})
+     */
+    private $active;
+
+    public function __construct() {
+        $this->created_at = new \DateTime();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getSlug(): string {
+        return (new Slugify())->slugify($this->title);
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getEditedAt(): ?\DateTimeInterface
+    {
+        return $this->edited_at;
+    }
+
+    public function setEditedAt(?\DateTimeInterface $edited_at): self
+    {
+        $this->edited_at = $edited_at;
+
+        return $this;
+    }
+
+    public function getActive(): ?bool
+    {
+        return $this->active;
+    }
+
+    public function setActive(bool $active): self
+    {
+        $this->active = $active;
+
+        return $this;
+    }
+}

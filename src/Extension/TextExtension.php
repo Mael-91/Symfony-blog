3ceada7de08d69d8d1e9ff9158d4317422fa0c42
Mael <contact.squeezo1@gmail.com>
2020-03-01 20:25:03 +0100

    Ajouts extension twig truncate

---
+++ src/Extension/TextExtension.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Extension;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class TextExtension extends AbstractExtension {
+
+    public function getFilters() {
+        return [
+            new TwigFilter('truncate', [$this, 'truncate'])
+        ];
+    }
+
+    public function truncate(?string $content, int $maxLenght = 200): string {
+        if (is_null($content)) {
+            return '';
+        }
+
+        if (mb_strlen($content) > $maxLenght) {
+            $truncate = mb_substr($content, 0, $maxLenght);
+            $lastSpace = mb_strripos($truncate, ' ');
+            return mb_substr($truncate, 0, $lastSpace) . ' ...';
+        }
+        return $content;
+    }
+}

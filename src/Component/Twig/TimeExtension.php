c6602a73adc0d27373b0845a6c7e1f31c84d64f1
Mael <contact.squeezo1@gmail.com>
2020-03-02 17:53:48 +0100

    Ajout de la library timeagojs, extension twig
    - Ajouts de la library timeagojs pour formter les dates
    - Ajouts de l'extension TimeExtension pour twig

---
+++ src/Component/Twig/TimeExtension.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace App\Component\Twig;
+
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class TimeExtension extends AbstractExtension {
+
+    public function getFilters()
+    {
+        return [
+            new TwigFilter('ago', [$this, 'ago'], ['is_safe' => ['html']])
+        ];
+    }
+
+    /**
+     * Permet d'envoyer à la vue, une date formatée
+     * @param \DateTime $date
+     * @param string|null $addClass
+     * @param string $format
+     * @return string
+     */
+    public function ago(\DateTime $date, string $addClass = null, string $format = 'd/m/Y H:i') {
+        return '<span class="timeago '. $addClass . '" datetime="' . $date->format(\DateTime::ISO8601) . '">' . $date->format($format) .'</span>';
+    }
+}

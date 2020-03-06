00794db5c5b04a2cd73ef97dd11c2e0bb13e8ba1
Mael <contact.squeezo1@gmail.com>
2020-03-06 16:45:12 +0100

    Suppression d'une fonction en commentaire

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -80,20 +80,6 @@ class SecurityController extends AbstractController {
         ]);
     }
 
-    /*public function login(AuthenticationUtils $authenticationUtils) {
-        if ($this->isGranted('IS_AUTHENTICATED_FULLY')) {
-            throw new \Exception('', 403);
-        }
-        $error = $authenticationUtils->getLastAuthenticationError();
-        $lastUsername = $authenticationUtils->getLastUsername();
-        return $this->render('pages/security/login.html.twig', [
-            'current_menu' => 'login',
-            'is_dashboard' => 'false',
-            'error' => $error,
-            'las_username' => $lastUsername
-        ]);
-    }*/
-
     /*public function changePassword() {
 
     }*/

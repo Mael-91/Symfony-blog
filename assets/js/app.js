cfe40cf1b656327b8e42c10af145bb4646c038c4
Mael <contact.squeezo1@gmail.com>
2020-03-18 01:23:01 +0100

    Ajout de WebPack

---
+++ assets/js/app.js
@@ -0,0 +1,20 @@
+import '../css/app.css';
+import 'jquery';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+import 'flatpickr';
+import { French } from 'flatpickr/dist/l10n/fr.js';
+
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
+require('flatpickr/dist/themes/dark.css');
+
+const $ = require('jquery');
+
+// Flatpickr extension
+flatpickr(".flatpickr", {
+    altInput: true,
+    altFormat: "d/m/Y",
+    dateFormat: "Y-m-d",
+    locale: French
+})

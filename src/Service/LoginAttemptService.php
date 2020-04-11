d928a8eac91d34f648a969b573c1111212f1316c
Mael <contact.squeezo1@gmail.com>
2020-04-11 19:05:31 +0200

    Fix: Class non appelées

--- src/Service/LoginAttemptService.php
+++ src/Service/LoginAttemptService.php
@@ -6,7 +6,6 @@ use App\Entity\LoginAttempt;
 use App\Entity\User;
 use App\Repository\LoginAttemptRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Psr\EventDispatcher\EventDispatcherInterface;
 
 class LoginAttemptService {
 
@@ -20,18 +19,12 @@ class LoginAttemptService {
      * @var LoginAttemptRepository
      */
     private $repository;
-    /**
-     * @var EventDispatcherInterface
-     */
-    private $dispatcher;
 
     public function __construct(
         EntityManagerInterface $manager,
-        LoginAttemptRepository $repository,
-        EventDispatcherInterface $dispatcher) {
+        LoginAttemptRepository $repository) {
         $this->manager = $manager;
         $this->repository = $repository;
-        $this->dispatcher = $dispatcher;
     }
 
     public function addAttempt(User $user): void {
